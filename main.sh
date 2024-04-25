3c81084bf63decd78c3ba712a782546931650f2e
Gaven Franco <102875948+GavenFranco@users.noreply.github.com>
2024-04-25 12:00:56 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 ## Created By Gaven Franco
 ## Disclaimer I am Not responsible for your actions. Please don't be stupid with this tool.
+## Wifi Cracker writen in bash.
 
-print "Welcom to bashFiCrack"
+menu() {
+  clear
+  echo"  
+    ____             __    _______ ______                __  
+   / __ )____ ______/ /_  / ____(_) ____/________ ______/ /__
+  / __  / __ `/ ___/ __ \/ /_  / / /   / ___/ __ `/ ___/ //_/
+ / /_/ / /_/ (__  ) / / / __/ / / /___/ /  / /_/ / /__/ ,<   
+/_____/\__,_/____/_/ /_/_/   /_/\____/_/   \__,_/\___/_/|_|
+
+"
+echo " ============Welcome to BashFiCrack============ "
+echo " 1). Set Interface to Monitor mode."
+echo " 2). Scan for networks."
+echo " 3). Deauth and Caputure"
+read -p "Enter your choice (1/2/3..): " choice
+
+case $choice in
+}
+
